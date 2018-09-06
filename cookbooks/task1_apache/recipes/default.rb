61e9234f39737ba85ee86bf87e3f2f62bcfbcd3d
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-09-06 19:35:12 +0300

    chore(chef): install apache cookbook

--- cookbooks/task1_apache/recipes/default.rb
+++ cookbooks/task1_apache/recipes/default.rb
@@ -1,5 +1,8 @@
-#
-# Cookbook:: task1_apache
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
